cdf8ed53562599a23ff0222e45d01a95227ae756
shittu adewale <sheriffhardewale@gmail.com>
2022-07-29 14:03:18 +0100

    added more logic

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ function App() {
      
       <div className="banner border-black border-1 w-full pb-12">
         <div className="bg-[#0a182c] border-black text-[#ccc3c3] text-4xl font-bold border-b p-6" >
-          JkWon Swap
+          JAILKWON Swap
         </div>
         <div className="steps p-4 py-6 md:p-8 flex justify-between">
           <h2 id="one" 
           // onClick={handleActiveStep} 
-          className= {activeStep === "one" ? "active border-[#EEABC4] cursor-pointer border-b-2 uppercase font-bold" : "font-bold text-gray-300 uppercase cursor-pointer"}  name="one" >Step 1</h2>
+          className= {activeStep === "one" ? "active border-[#EEABC4] cursor-pointer border-b-4 uppercase font-bold" : "font-bold text-gray-300 uppercase cursor-pointer"}  name="one" >Step 1</h2>
           <h2 id="two" 
-          className= {activeStep === "two" ? "active border-[#EEABC4] cursor-pointer border-b-2 uppercase font-bold" : "font-bold text-gray-300 cursor-pointer uppercase"}
+          className= {activeStep === "two" ? "active border-[#EEABC4] cursor-pointer border-b-4 uppercase font-bold" : "font-bold text-gray-300 cursor-pointer uppercase"}
           // onClick={handleActiveStep} 
           
           name="two" >Step 2</h2>
           <h2 id="three" 
-          className= {activeStep === "three" ? "active border-[#EEABC4] cursor-pointer border-b-2 uppercase font-bold" : "font-bold text-gray-300 uppercase cursor-pointer"}
+          className= {activeStep === "three" ? "active border-[#EEABC4] cursor-pointer border-b-4 uppercase font-bold" : "font-bold text-gray-300 uppercase cursor-pointer"}
           // onClick={handleActiveStep}
            >Step 3</h2>
         </div>
